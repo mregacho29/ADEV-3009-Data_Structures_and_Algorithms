328451a41a6d4b8878d7313fe08560d681744970
mregacho <143650376+mregacho29@users.noreply.github.com>
2025-01-26 16:46:48 -0600

    Assignment 1 - Milestone 1

---
+++ A1_MaCrizzaRegacho/A1_MaCrizzaRegacho/Employee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+
+namespace A1_MaCrizzaRegacho
+{
+    public class Employee : IComparable<Employee>
+    {
+
+        // Properties for EmployeeId, FirstName, and LastName
+        // These properties are read-only, meaning they can only be set through the constructors
+        public int EmployeeID { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        /// <summary>
+        /// Constructor that initializes only the EmployeeId
+        /// </summary>
+        /// <param name="employeeId">The ID of the Employee</param>
+        public Employee(int employeeId)
+        {
+            // Set the EmployeeId property
+            EmployeeID = employeeId;
+
+        }
+
+        /// <summary>
+        /// Constructor that initializes the EmployeeId, FirstName, and LastName
+        /// </summary>
+        /// <param name="employeeId">Employee ID</param>
+        /// <param name="firstName">First Name</param>
+        /// <param name="lastName">Last Name</param>
+        public Employee(int employeeId, string firstName, string lastName)
+        {
+            // Set the EmployeeId property
+            EmployeeID = employeeId;
+
+            // Set the FirstName and LastName properties
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        /// <summary>
+        /// Compare two Employee objects based on their EmployeeId
+        /// </summary>
+        /// <param name="other">other</param>
+        /// <returns></returns>
+        public int CompareTo(Employee other)
+        { 
+            if (other == null) return 1;
+            return EmployeeID.CompareTo(other.EmployeeID);
+        }
+
+        /// <summary>
+        /// Override the ToString method to return the EmployeeId, FirstName, and LastName
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        { 
+            string firstName = FirstName ?? "null";
+            string lastName = LastName ?? "null";
+            return $"{EmployeeID} {firstName} {lastName}";
+        }
+    }
+}
---
+++ A1_MaCrizzaRegacho/A1_MaCrizzaRegacho/Node.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+
+namespace A1_MaCrizzaRegacho
+{
+
+    public class Node<T>
+    {
+        // Properties
+        public T Element { get; set; }
+        public Node<T> Next { get; set; }
+        public Node<T> Previous { get; set; }
+
+        /// <summary>
+        /// Constructor that initializes fields to default values
+        /// </summary>
+        public Node()
+        {
+            Element = default(T);
+            Next = null;
+            Previous = null;
+        }
+
+        /// <summary>
+        /// Constructor that initializes only the element property
+        /// </summary>
+        /// <param name="element">Element</param>
+        public Node(T element)
+        {
+            Element = element;
+            // Next and Previous are implicitly set to null by default
+        }
+
+        /// <summary>
+        /// Constructor that initializes all properties
+        /// </summary>
+        /// <param name="element">Element</param>
+        /// <param name="previousNode">Previous Node</param>
+        /// <param name="nextNode">Next Node</param>
+        public Node(T element, Node<T> previousNode, Node<T> nextNode)
+        {
+            Element = element;
+            Previous = previousNode;
+            Next = nextNode;
+        }
+
+    }
+}
