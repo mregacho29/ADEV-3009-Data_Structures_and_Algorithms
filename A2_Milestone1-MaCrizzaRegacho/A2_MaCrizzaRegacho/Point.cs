dfa329eea7bf21329eab62ad098f93e71eb4f80f
mregacho <143650376+mregacho29@users.noreply.github.com>
2025-03-09 19:38:22 -0500

    Assignment 2: Milestone 2 - Stack

---
+++ A2_Milestone1-MaCrizzaRegacho/A2_MaCrizzaRegacho/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2_MaCrizzaRegacho
+{
+    public class Point
+    {
+
+        public int Row { get; }
+        public int Column { get; }
+
+        public Point(int row, int column) => (Row, Column) = (row, column);
+
+        public override string ToString() => $"[{Row},{Column}]";
+
+    }
+}
